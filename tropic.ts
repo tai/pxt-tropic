0ccfff21c765a33ebe35b5d957b86754042676eb
Taisuke Yamada <tai@users.noreply.github.com>
2023-04-10 16:45:39 +0900

    initial commit

---
+++ tropic.ts
@@ -0,0 +1,39 @@
+
+/**
+* このファイルを使って、独自の関数やブロックを定義してください。
+* 詳しくはこちらを参照してください：https://makecode.microbit.org/blocks/custom
+*/
+
+enum MyEnum {
+    //% block="one"
+    One,
+    //% block="two"
+    Two
+}
+
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#0fbc11 icon=""
+namespace tropic {
+    /**
+     * TODO: describe your function here
+     * @param n describe parameter here, eg: 5
+     * @param s describe parameter here, eg: "Hello"
+     * @param e describe parameter here
+     */
+    //% block
+    export function foo(n: number, s: string, e: MyEnum): void {
+        // Add code here
+    }
+
+    /**
+     * Add 2 numbers
+     * @param a value to add
+     * @param b value to add
+     */
+    //% block
+    export function add(a: number, b: number): number {
+        return a + b;
+    }
+}
